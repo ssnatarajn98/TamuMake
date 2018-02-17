76c861f3e830f91d7a6e7972d8c558932779e3ac
sriramNatarajan <ssnatarajan98@gmail.com>
2018-02-17 02:00:06 -0600

    version 2

--- Leap Motion/GetGestures.py
+++ Leap Motion/GetGestures.py
@@ -6,9 +6,18 @@ arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 #arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+class SampleListener(Leap.Listener):
+
+    def on_connect(self, controller):
+        print "Connected"
+
+
+    def on_frame(self, controller):
+        print "Frame available"
 
 def main():
-
+    listener = SampleListener()
+    controller = Leap.Controller()
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
     try:
