f3b4227c846b245037c6b43b6b03b88cc6b57c42
Yamen Al Masalmeh <YamenAlMasalmeh@gmail.com>
2018-02-17 02:43:58 -0600

    Version 3

--- Leap Motion/GetGestures.py
+++ Leap Motion/GetGestures.py
@@ -6,16 +6,34 @@ arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 #arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+class GestureListener(Leap.Listener):
+
+    def on_connect(self, controller):
+        print "Connected"
+
+
+    def on_frame(self, controller):
+        print "Frame available"
+
+
+    def on_disconnect(self, controller):
+        print "Disconnected"
+
 
 def main():
 
+    controller = Leap.Controller()
     # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
-    try:
-        sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
+    while True:
+        frame = controller.frame()
+        hands = frame.hands
+        fingers = frame.fingers
+
+
+        for hand in hands:
+            print hand.grab_strength
+        time.sleep(0.2)
+
 
 if __name__ == "__main__":
     main()
-
